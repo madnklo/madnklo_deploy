3af400858966913cfeeb8ed9e0c587844b689f0b
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2019-08-14 11:15:26 +0200

    proper behavior for out-of-the-box test (persistent folder at $(pwd))

--- docker_mg5.sh
+++ docker_mg5.sh
@@ -8,7 +8,11 @@ fi
 
 if [ "$1"="--set_persistent" ] && [[ ! -z "$2"  ]]
 then
+	echo ""
+	echo "=============================================="
 	echo "This command must be sourced to have an effect"
+	echo "=============================================="
+	echo ""	
 	export DOCKER_MG5_PERSISTENT="$2"
 	exit
 fi
@@ -18,7 +22,8 @@ then
 	echo "The DOCKER_MG5_PERSISTENT variable is not set."
 	echo "Setting the current folder"
 	echo "You can set it by running"
-	echo "$>source docker_mg5.sh --set_persistent PATH"
+	echo "source docker_mg5.sh --set_persistent PATH"
+	DOCKER_MG5_PERSISTENT=$(pwd)
 fi
 	
 docker run -it --rm -v $DOCKER_MG5_PERSISTENT:/var/mg5_persistent ndeutschmann/madnklo $1
